ed4010c2732856919301902e0c989ac069ee5e95
Ben Watt <77812385+continualWolf@users.noreply.github.com>
2022-09-25 08:34:51 -0700

    Get all the image tags from every page on a site

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using HtmlAgilityPack;
+using System.Net;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 //Notes:
 // I have learnt how to scrape the web through the following website -> https://medium.com/c-sharp-progarmming/create-your-own-web-scraper-in-c-in-just-a-few-minutes-c42649adda8
@@ -9,16 +13,26 @@
 HtmlWeb web = new HtmlWeb();
     
     //load a specific website from web
-    HtmlDocument doc = web.Load("https://en.wikipedia.org/wiki/Anime");
+    HtmlDocument doc = web.Load("https://www.averticalworld.co.uk/");
+//Console.WriteLine(doc.DocumentNode.InnerHtml);
 
-    //retreive all span elements that have the class 'toctext'
-    var HeaderNames =
-        doc.DocumentNode.SelectNodes("//span[@class='toctext']");
-    
-    //loop through headers and print
-    foreach (var item in HeaderNames)
+//retreive nav bar items
+var NavItems =
+    doc.DocumentNode.SelectNodes("//span[@class='menu-text']");
+
+foreach (var item in NavItems.Distinct().ToList())
+{
+    string url = "https://www.averticalworld.co.uk/" + item.InnerText.ToLower();
+
+    HtmlDocument tempDoc = web.Load(url);
+
+    if (tempDoc == null) continue;
+
+    var images =
+        tempDoc.DocumentNode.SelectNodes("//img");
+
+    foreach (var image in images)
     {
-        Console.WriteLine(item.InnerText);
+        Console.WriteLine(image.OuterHtml);
     }
-
-
+}
